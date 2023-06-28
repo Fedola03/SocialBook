75d387ff7e8eeb7f4879e96de7c7661a39f6d3dc
Adugbo Efemena <efeadugbo3@gmail.com>
2023-06-28 15:14:42 +0100

    account settings III

--- social_book/settings.py
+++ social_book/settings.py
@@ -56,7 +56,8 @@ ROOT_URLCONF = 'social_book.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'),
+                 os.path.join(BASE_DIR, 'media')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
