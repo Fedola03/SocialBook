69b31c8883935738cbee3d8f860e12af5a8453cb
Adugbo Efemena <efeadugbo3@gmail.com>
2023-07-10 17:55:33 +0100

    Like-settings III

--- core/models.py
+++ core/models.py
@@ -32,3 +32,6 @@ class Post(models.Model):
 class LikePost(models.Model):
     post_id = models.CharField(max_length=100)
     username = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.username
